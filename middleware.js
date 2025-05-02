8995679a4c01e4043eb15eae2108a78e0b8f492b
Sharjun Hussain <sharjoonibnuhussain99@gmail.com>
2025-05-03 00:37:32 +0530

    fix:commented middeware code

--- middleware.js
+++ middleware.js
@@ -2,18 +2,18 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const token = request.cookies.get("accessToken");
+  // const token = request.cookies.get("accessToken");
 
-  const protectedPaths = ["/dashboard", "/profile", "/admin"];
-  const pathname = request.nextUrl.pathname;
+  // const protectedPaths = ["/dashboard", "/profile", "/admin"];
+  // const pathname = request.nextUrl.pathname;
 
-  const isProtected = protectedPaths.some((path) => pathname.startsWith(path));
+  // const isProtected = protectedPaths.some((path) => pathname.startsWith(path));
 
-  if (isProtected && !token) {
-    const url = request.nextUrl.clone();
-    url.pathname = "/login";
-    return NextResponse.redirect(url);
-  }
+  // if (isProtected && !token) {
+  //   const url = request.nextUrl.clone();
+  //   url.pathname = "/login";
+  //   return NextResponse.redirect(url);
+  // }
 
   return NextResponse.next();
 }
